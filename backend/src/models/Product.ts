6b4d3ac93d3d179211a061b2ba26dcc20cdd6675
jakob753951 <jlm1999@live.dk>
2024-03-12 10:26:59 +0100

    Merged different productTypes into a single Product class

--- backend/src/models/Product.ts
+++ backend/src/models/Product.ts
@@ -1,18 +1,18 @@
 import { ProductType } from "./ProductType";
 
-export class PlayStation {
+export class Product {
   public title: string;
   public imageUrl: string;
   public basePrice: number;
   public taxRate: number = 1.25;
+  public discountRate: number = 0.1;
+  public productType: ProductType;
 
-  public discountRate: number = 0.15;
-  public productType: ProductType = ProductType.PlayStation;
-
-  constructor(title: string, imageUrl: string, basePrice: number) {
+  constructor(title: string, imageUrl: string, basePrice: number, productType: ProductType) {
     this.title = title;
     this.imageUrl = imageUrl;
     this.basePrice = basePrice;
+    this.productType = productType;
   }
 
   public getPrice(): number {
