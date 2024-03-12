33592a855fd639abedb61500ce320e749b31e04a
jakob753951 <jlm1999@live.dk>
2024-03-12 11:45:26 +0100

    added feature-enable to dis/enable unfinished-feature endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import { recommendedProducts } from './dummyData';
 const app = express();
 const port = 3001;
 const cors = require('cors');
+let feature_enabled = true;
 app.use(cors());
 
 app.use(express.json());
@@ -32,8 +33,20 @@ app.get("/get-products-by-category", (req: Request, res: Response) => {
 });
 
 app.get("/unfinished-feature", (_: Request, res: Response) => {
+  if (!feature_enabled) {
+    return res.status(404).send("No features here ;D")
+  }
   // Oh no, this feature is not ready for production!
   return res.status(500).send('Internal Server Error');
+});
+
+app.post("/feature-enable", (req: Request, res: Response) => {
+  try {
+    feature_enabled = req.body.enable;
+    return res.status(200).send(`feature switched to ${req.body.enable}`);
+  } catch (e) {
+    return res.status(400).send("body must be 'true' or 'on' to enable the feature, or 'false' or 'off' to disable it");
+  }
 });
 
 app.listen(port, () => {
